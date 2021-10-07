29102393cfb1226fd4358669bc9acd130a445570
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2021-10-07 15:46:10 -0400

    Adding ts api endpoint example

---
+++ app/pages/api/hello.ts
@@ -0,0 +1,12 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+//TypeScript example
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  res.status(200).json({ name: 'John Doe' })
+}
