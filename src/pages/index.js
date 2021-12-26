56745b8aedb0d66f90629b8301a444e5f276d9a3
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2021-12-26 10:42:14 -0500

    imports for scss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/pages/Home.module.scss'
 
 export default function Home() {
   return (
