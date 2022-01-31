c7e566993086d3837b15802cf452bd39136a4cd5
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-01-31 11:18:20 -0500

    modified api structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,37 @@
-
 /**
  * React header component.
  * @file navigational header component.
  * @author Nicholas Sliter,
- * 
+ *
  */
 
 import Link from "next/link";
 import LoginButton from "./common/LoginButton";
 
-
 import styles from "../styles/components/Header.module.scss";
 
-
-
 export default function Header() {
-
-   return (
-     <header className={styles.header}>
-       <Link href="/">
-         <img
-           src="/middcourses_logo.svg"
-           alt="middCourses"
-           draggable="false"
-           className={styles.logo}
-         />
-       </Link>
-       <div className={styles.nav_bar}>
-         <li>
-           <a href="/">Home</a>
-           <a href="/about">About</a>
-         </li>
-         <LoginButton header />
-       </div>
-     </header>
-   );
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <img
+          src="/middcourses_logo.svg"
+          alt="middCourses"
+          draggable="false"
+          className={styles.logo}
+        />
+      </Link>
+      <div className={styles.nav_bar}>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <LoginButton header />
+      </div>
+    </header>
+  );
 }
---
+++ src/pages/api/reviews/[reviewId]/index.ts
@@ -0,0 +1,31 @@
+import nc from "next-connect";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = nc({
+   onError: (err, req: NextApiRequest, res: NextApiResponse) => {
+      console.log(err.stack);
+      res.status(500).end("Something went wrong");
+   },
+   onNoMatch: (req: NextApiRequest, res: NextApiResponse) => { 
+      res.status(404).end("Not Found");
+   }
+})
+
+
+.get((req: NextApiRequest, res: NextApiResponse) => {
+   
+   res.status(200).json({
+      message: "Hello World"
+   });
+
+
+})
+
+.post((req: NextApiRequest, res: NextApiResponse) => {
+   
+
+
+});
+
+
+export default handler;
