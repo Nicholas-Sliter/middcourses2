4786faf48227709c74927a90b64166be43665971
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-02-08 10:28:52 -0500

    begin seed file, add slug to instructor table, and begin creation of knex down functions

--- migrations/20220119150301_course_data.js
+++ migrations/20220119150301_course_data.js
@@ -1,51 +1,49 @@
-exports.up = function(knex) {
-  return knex.schema.createTable("Course", (table) => {
-    table
-      .string("courseID")
-      .notNullable(); /* Course ID: 4-digit department identifier followed by course number, eg. CSCI0201  */
-    table
-			.string("courseDepartmentID")
-			.notNullable(); /* Course Department: Department abbreviation */
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("Course", (table) => {
       table
-      .string("courseName")
-      .notNullable(); /* Course Name: Course name */
-		table
-			.text("courseDescription")
-			.notNullable(); /* Course Description: Course description */
-  })
-  .createTable("Instructor", (table) => {
-    table
-      .string("name")
-      .notNullable();
-    table
-      .string("instructorID")
-      .notNullable();
-  })
-  .createTable("CourseInstructor", (table) => {
-    table
-      .string("courseID")
-      .notNullable();
+        .string("courseID")
+        .notNullable(); /* Course ID: 4-digit department identifier followed by course number, eg. CSCI0201  */
       table
-      .string("instructorID")
-      .notNullable();
-    table
-      .foreign("courseID")
-      .references("Course.courseID")
-      .onDelete("CASCADE");
-    table
-      .foreign("instructorID")
-      .references("Instructor.instructorID")
-      .onDelete("CASCADE");
-  })
-	.createTable("Department", (table) => {
-		table
-			.string("departmentID", 4)
-			.unique()
-			.notNullable(); /* Department ID: 4-letter department abbreviation */
-		table
-			.string("departmentName")
-			.notNullable(); /* Department Name: Department name */
-	});
-}
+        .string("courseDepartmentID")
+        .notNullable(); /* Course Department: Department abbreviation */
+      table.string("courseName").notNullable(); /* Course Name: Course name */
+      table
+        .text("courseDescription")
+        .notNullable(); /* Course Description: Course description */
+    })
+    .createTable("Instructor", (table) => {
+      table.string("name").notNullable();
+      table.string("slug").unique().notNullable();
+      table.string("instructorID").unique().notNullable();
+    })
+    .createTable("CourseInstructor", (table) => {
+      table.string("courseID").notNullable();
+      table.string("instructorID").notNullable();
+      table
+        .foreign("courseID")
+        .references("Course.courseID")
+        .onDelete("CASCADE");
+      table
+        .foreign("instructorID")
+        .references("Instructor.instructorID")
+        .onDelete("CASCADE");
+    })
+    .createTable("Department", (table) => {
+      table
+        .string("departmentID", 4)
+        .unique()
+        .notNullable(); /* Department ID: 4-letter department abbreviation */
+      table
+        .string("departmentName")
+        .notNullable(); /* Department Name: Department name */
+    });
+};
 
-exports.down = function(knex) {}
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("Course")
+    .dropTableIfExists("Instructor")
+    .dropTableIfExists("CourseInstructor")
+    .dropTableIfExists("Department");
+};
