efc4bcc3cfc508e3139bb90a0709965a0c62d8ea
Rohan Datar <37545071+rohan-datar@users.noreply.github.com>
2022-02-25 12:06:13 -0500

    added script to process instructor data from catalog

--- src/lib/backend/scripts.ts
+++ src/lib/backend/scripts.ts
@@ -1,8 +1,10 @@
 import { public_course } from "../common/types";
+import { Scraper } from "directory.js";
 
 /* eslint-disable */
 export default {};
 
+//test inteface to simulate data from catalog
 interface CourseObject {
     code: string;
     description: string;
@@ -56,6 +58,68 @@ const processCourses = (rawCourses:CourseObject[]) => {
 
     return (formattedCourses);
 
+
+}
+
+//test interface in order to simulate data from catalog
+interface InstructorObject {
+    id: string
+    name: string
+};
+
+//interface to store instructor data in database
+interface Instructor {
+    name: string;
+    slug: string;
+    instructorID: string;
+    departmentID: string;
+};
+
+//extract 
+const getInstructorData = async (rawInstructor: InstructorObject) => {
+
+    let formattedInstructor:Instructor;
+
+    formattedInstructor.name = rawInstructor.name;
+
+    formattedInstructor.instructorID = rawInstructor.id;
+
+    //gets person from directory by looking up their id
+    const S = new Scraper("", formattedInstructor.instructorID);
+    await S.init();
+    const department = S.person.department; //this gets department name,  NOT departmentID, but I am using this a placeholder until a name-department ID dictionary can be implemented
+
+    formattedInstructor.departmentID = department;
+
+    return (formattedInstructor);
+
+}
+
+const processInstructors =  (rawInstructors:InstructorObject[]) => {
+
+    //create dictionary to keep track of duplicate instructors
+    const instructorDict = {};
+
+    //store processed instructors in an array
+    const formattedInstructors:Instructor[] = [];
+
+    rawInstructors.forEach( async (rawInstructor) => {
+
+        let formattedInstructor = await getInstructorData(rawInstructor);
+
+        const ID = formattedInstructor.instructorID;
+
+        if (!instructorDict[ID]){
+
+            instructorDict[ID] = 1;
+
+            formattedInstructors.push(formattedInstructor);
+
+        }
+
+    })
+
+    return(formattedInstructors);
 
 }
 
