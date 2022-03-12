d4da89b1bcd8284de0da0051a0aa97faab8356ec
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-03-12 16:19:35 -0500

    added Google gtm for analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,21 @@ import type { AppProps /*, AppContext */ } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { getProviders } from "next-auth/react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
+import {TagManager} from 'react-gtm-module'
+import { useEffect } from "react";
 
 delete theme.styles.global;
 
 import HeaderFooterLayout from "../layouts/HeaderFooterLayout";
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: 'GTM-WJDFWKT',
+    })
+  }, [])
+
   return (
     <SessionProvider session={pageProps.session}>
       <ChakraProvider resetCSS={false}>
