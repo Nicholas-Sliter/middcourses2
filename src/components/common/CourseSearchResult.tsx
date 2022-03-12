f7d770da05fdddea1032873bc1316bfc8c7d11f7
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-03-12 12:02:36 -0500

    attempt routing fix for webkit-based browsers

--- src/components/common/CourseSearchResult.tsx
+++ src/components/common/CourseSearchResult.tsx
@@ -1,7 +1,7 @@
 import { public_course } from "../../lib/common/types";
 import { MenuItem } from "@chakra-ui/react";
 import { FaBook } from "react-icons/fa";
-import Router from "next/router";
+import {useRouter} from "next/router";
 import styles from "../../styles/components/common/SearchBar.module.scss";
 
 
@@ -15,12 +15,15 @@ export default function CourseSearchResult({
   compact = true,
 }: CourseSearchResultProps) {
 
+
+  const router = useRouter();
+
   return (
     <MenuItem 
       key={course.courseID}
       icon={<FaBook />}
       onClick={() => {
-        Router.push(
+        router.push(
           `/reviews/${course.courseID
             .substring(0, 4)
             .toLowerCase()}/${course.courseID.substring(4)}`
