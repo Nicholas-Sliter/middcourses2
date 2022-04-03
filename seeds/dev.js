5310a4bb90d6e3a910000d5f7135acf9cc1dc539
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-04-03 13:45:09 -0400

    fixed seeding

--- seeds/dev.js
+++ seeds/dev.js
@@ -1,64 +1,70 @@
-const fs = require('fs');
+const fs = require("fs");
 
 //chunk size for batch insert
 const MAX_TEST_DATA = 100;
 
-exports.seed = async function(knex) {
-
-    //Delete ALL existing table entries
-   await knex("User").del();
-   await knex("Course").del();
-   await knex("Review").del();
-   await knex("Flagged").del();
-   await knex("Instructor").del();
-   await knex("Department").del();
-   await knex("CourseInstructor").del();
+exports.seed = async function (knex) {
+  //Delete ALL existing table entries
+  await knex("User").del();
+  await knex("Course").del();
+  await knex("Review").del();
+  await knex("Flagged").del();
+  await knex("Instructor").del();
+  await knex("Department").del();
+  await knex("CourseInstructor").del();
 
   //Insert seed entries
-   const courseData = JSON.parse(fs.readFileSync('./data/Course.json', 'utf8'));
-   const courses = courseData.map((course) => {
-      return {
-         courseID: course.courseID,
-         courseName: course.courseName,
-         courseDescription: course.courseDescription,
-         departmentID: course.departmentID ?? course.courseID.slice(0,4).toUpperCase()
-      };
 
-   });
-   await knex.batchInsert("Course", courses, MAX_TEST_DATA);
+  const departments = JSON.parse(
+    fs.readFileSync("./data/Department.json", "utf8")
+  );
+  await knex.batchInsert("Department", departments, MAX_TEST_DATA);
 
-   console.log(await knex("Course").select("courseID"));
+  const courseData = JSON.parse(fs.readFileSync("./data/Course.json", "utf8"));
+  const courses = courseData.map((course) => {
+    return {
+      courseID: course.courseID,
+      courseName: course.courseName,
+      courseDescription: course.courseDescription,
+      departmentID:
+        course.departmentID ?? course.courseID.slice(0, 4).toUpperCase(),
+    };
+  });
+  await knex.batchInsert("Course", courses, MAX_TEST_DATA);
 
-   const instructors = JSON.parse(fs.readFileSync('./data/Instructor.json', 'utf8'));
-   const fixedInstructors = instructors.map(instructor => {
-     return({
-       instructorID: instructor.instructorID,
-        name: instructor.name,
-        slug: instructor.slug,
-        departmentID: instructor.departmentID ?? ""
-     });
-    });
+  console.log(await knex("Course").select("courseID"));
 
-   await knex.batchInsert("Instructor", fixedInstructors, MAX_TEST_DATA);
+  const instructors = JSON.parse(
+    fs.readFileSync("./data/Instructor.json", "utf8")
+  );
+  const fixedInstructors = instructors.map((instructor) => {
+    return {
+      instructorID: instructor.instructorID,
+      name: instructor.name,
+      slug: instructor.slug,
+      departmentID: instructor.departmentID ?? "",
+    };
+  });
 
-   console.log(await knex("Instructor").select("instructorID"));
+  await knex.batchInsert("Instructor", fixedInstructors, MAX_TEST_DATA);
 
-   const courseInstructors = JSON.parse(fs.readFileSync('./data/CourseInstructor.json', 'utf8'));
+  console.log(await knex("Instructor").select("instructorID"));
 
-   //await knex.batchInsert("CourseInstructor", courseInstructors, MAX_TEST_DATA);
-   //much slower
-   courseInstructors.forEach(async (courseInstructor) => {
-      knex("CourseInstructor").insert(courseInstructor).catch(err => console.log(err));
-    });
+  const courseInstructors = JSON.parse(
+    fs.readFileSync("./data/CourseInstructor.json", "utf8")
+  );
 
-   const departments = JSON.parse(fs.readFileSync('./data/Department.json', 'utf8'));
-   await knex.batchInsert("Department", departments, MAX_TEST_DATA);
+  //await knex.batchInsert("CourseInstructor", courseInstructors, MAX_TEST_DATA);
+  //much slower
+  courseInstructors.forEach(async (courseInstructor) => {
+    knex("CourseInstructor")
+      .insert(courseInstructor)
+      .catch((err) => console.log(err));
+  });
 
-   const users = JSON.parse(fs.readFileSync('./data/User.json', 'utf8'));
-   await knex.batchInsert("User", users, MAX_TEST_DATA);
+  const users = JSON.parse(fs.readFileSync("./data/User.json", "utf8"));
+  await knex.batchInsert("User", users, MAX_TEST_DATA);
 
-   const reviews = JSON.parse(fs.readFileSync('./data/Review.json', 'utf8'));
-   await knex.batchInsert("Review", reviews, MAX_TEST_DATA);
-
-
+  const reviews = JSON.parse(fs.readFileSync("./data/Review.json", "utf8"));
+  await knex.batchInsert("Review", reviews, MAX_TEST_DATA);
 };
