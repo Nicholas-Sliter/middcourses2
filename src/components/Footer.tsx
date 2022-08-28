3a0b764f18f1a98391b27cb7a080b00157c09c21
Nicholas Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-08-28 19:29:45 -0400

    Hide footer elements on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,15 @@
 import styles from "../styles/components/Footer.module.scss";
+import useMobile from "../hooks/useMobile";
 
 export default function Footer() {
+  const isMobile = useMobile();
+
   return (
     <>
       <footer className={styles.footer}>
         <div>
           <span>
-            <a>Middlebury Course Reviews</a>
+            <a style={{ display: ((isMobile) ? 'none' : 'inline-block') }}>Middlebury Course Reviews</a>
             <a href="/privacy-policy.html">Privacy Policy</a>
           </span>
         </div>
