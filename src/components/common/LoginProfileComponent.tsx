1ef4cd9753edf26ad9f5b9c31a03c83f0b36c38e
Nicholas Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-09-06 10:21:08 -0400

    Fix type bug

--- src/components/common/LoginProfileComponent.tsx
+++ src/components/common/LoginProfileComponent.tsx
@@ -1,16 +1,16 @@
 import { useSession } from "next-auth/react";
-import { Session } from "../../lib/common/types";
+import { CustomSession } from "../../lib/common/types";
 import LoginButton from "./LoginButton";
 import ProfileButton from "./ProfileButton";
 
 
 export default function LoginProfileComponent() {
-  const { data:session } = useSession() as {data: Session};
+  const { data: session } = useSession() as { data: CustomSession };
 
   return (
-    <> 
-    {session?.user ? <ProfileButton /> : <LoginButton header={true} />} 
-    
+    <>
+      {session?.user ? <ProfileButton /> : <LoginButton header={true} />}
+
     </>
   );
 
