e4b63eb82aee83c9f6733e79cb78e0e80501b094
Nicholas Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-09-06 13:57:18 -0400

    Refactor: code cleanup in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,6 @@
-/**
- * React header component.
- * @file navigational header component.
- * @author Nicholas Sliter,
- *
- */
-
 import Link from "next/link";
-import LoginButton from "./common/LoginButton";
-
 import styles from "../styles/components/Header.module.scss";
-import ProfileButton from "./common/ProfileButton";
 import LoginProfileComponent from "./common/LoginProfileComponent";
-
-
-
-//TODO: login button should render the button if the user is not logged in
-// else it should render a user icon with a dropdown menu
 
 
 export default function Header() {
@@ -29,18 +14,6 @@ export default function Header() {
           className={styles.logo}
         />
       </Link>
-      {/**<div className={styles.nav_bar}>
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-        <LoginButton header />
-  </div>*/}
-      {/**<ProfileButton />*/}
       <LoginProfileComponent />
     </header>
   );
