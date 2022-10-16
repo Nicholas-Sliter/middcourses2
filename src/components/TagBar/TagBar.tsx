3e4f0e57e6480441e04e62618308740f7c17d9f4
Nicholas Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-10-16 16:01:07 -0400

    Feature: added group tagbar

--- src/components/TagBar/TagBar.tsx
+++ src/components/TagBar/TagBar.tsx
@@ -3,22 +3,27 @@ import styles from "./TagBar.module.scss";
 
 interface TagBarProps {
     items: string[];
-    tagClick?: (tag: string) => void;
+    tagClick?: (tag: string, tagGroup?: string[]) => void;
     tagClassName?: string;
     selectedTags?: string[];
     selectedTagClassName?: string;
+    hasGroup?: boolean;
 }
 
 
 function TagBar({
     items,
-    tagClick = (item) => { },
+    tagClick = (item, []) => { },
     tagClassName = "",
     selectedTags = [],
     selectedTagClassName = "",
+    hasGroup = false,
 }: TagBarProps) {
 
     const selectedTagsSet = new Set(selectedTags);
+
+    const clickFun = (hasGroup) ? (tag, items) => tagClick(tag, items) : (tag, items) => tagClick(tag, []);
+
 
     return (
         <div className={styles.tagBar}>
@@ -27,7 +32,7 @@ function TagBar({
                 const tagClass = isSelected ? selectedTagClassName : tagClassName;
 
                 return (
-                    <Tag key={`${i}:${item}`} onClick={() => tagClick(item)} className={tagClass}>
+                    <Tag key={`${i}:${item}`} onClick={() => clickFun(item, items)} className={tagClass}>
                         {item}
                     </Tag>)
             }
