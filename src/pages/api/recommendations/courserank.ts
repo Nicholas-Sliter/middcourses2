dc4e0833e77be7b2a9030f88cfaec3fa3314e81e
Nicholas Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-12-12 09:11:14 -0500

    Feature: new recs endpoint for COURSERANK

---
+++ src/pages/api/recommendations/courserank.ts
@@ -0,0 +1,24 @@
+/* Endpoint for getting course recommendations based on the COURSERANK algorithm */
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from "next-auth/react";
+import { getRecommendationsForUser } from '../../../lib/backend/recommendations';
+import { CustomSession } from '../../../lib/common/types';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+
+    const session = await getSession({ req }) as CustomSession;
+    const userID = req.query.userid as string;
+
+    if (userID && session?.user?.id !== userID && !session?.user?.admin) {
+        res.status(401).end("Not authorized");
+        return;
+    }
+
+    const recs = await getRecommendationsForUser(session);
+
+    res.status(200).end(JSON.stringify(recs)); //tmp (just courseID)
+
+}
+
+export default handler;
