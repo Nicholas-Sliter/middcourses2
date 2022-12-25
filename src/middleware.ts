4ea6d47eadd6caad418842e5851c5157cccc7c09
Nicholas Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-12-25 10:57:50 -0500

    Feature: heroku url redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ type Environment = "production" | "development" | "other";
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
     const currentEnv = process.env.NODE_ENV as Environment;
 
+    /* HTTPS redirect */
     if (
         currentEnv === "production" &&
         req.headers.get("x-forwarded-proto") !== "https"
@@ -13,5 +14,15 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
             301
         );
     }
+
+    /* Heroko URL redirect */
+    if (currentEnv == "production" && process.env.HEROKU_APP_NAME && req.headers.get("host") === `${process.env.HEROKU_APP_NAME}.herokuapp.com`) {
+        return NextResponse.redirect(
+            `https://${process.env.HOST_NAME}${req.nextUrl.pathname}`,
+            301
+        );
+    }
+
+
     return NextResponse.next();
 }
