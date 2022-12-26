dbb37a591e755b631b5b99d8656e64a548c17a2f
Nicholas Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-12-26 12:49:41 -0500

    Feature: dynamic server-generated ranking categories

--- src/pages/api/testapi5.ts
+++ src/pages/api/testapi5.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getBaseCourseAverages, getTopEasyAndValuableCourses } from "../../lib/backend/database/rankings";
+import { getCoursesInformation } from "../../lib/backend/database/course";
+import { getBaseCourseAverages, getChallengingCourses, getEasiestGoodCourses, getLowTimeCommitmentCourses, getTopEasyAndValuableCourses } from "../../lib/backend/database/rankings";
+import { public_course } from "../../lib/common/types";
 
 interface Course {
   courseID: string;
@@ -12,10 +14,89 @@ type Data = {
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
-  const aggregateData = await getBaseCourseAverages();
-  const courses: Course[] = getTopEasyAndValuableCourses(aggregateData, 10);
 
-  res.status(200).json({ courses });
+  const recommendationTypeMap = {
+    "easyValuable": {
+      func: getTopEasyAndValuableCourses,
+      params: {
+        limit: 10
+      },
+      title: "Easy and Valuable",
+      description: "Courses that are easy and valuable",
+    },
+    "easy": {
+      func: getEasiestGoodCourses,
+      params: {
+        limit: 10
+      },
+      title: "Nice for a break",
+      description: "Courses that are easy",
+    },
+    "lowTimeCommitment": {
+      func: getLowTimeCommitmentCourses,
+      params: {
+        limit: 10
+      },
+      title: "Low Time Commitment",
+      description: "Courses that are low time commitment",
+    },
+    "goodForAChallenge": {
+      func: getChallengingCourses,
+      params: {
+        limit: 10
+      },
+      title: "Good for a challenge",
+      description: "Courses that are good for a challenge",
+    },
+
+  };
+
+
+  const thresholdString = req.query.threshold ?? '3';
+  const threshold = parseInt(thresholdString as string, 10);
+
+  const aggregateData = await getBaseCourseAverages(threshold);
+  const rankedCourses = Object.fromEntries(Object.keys(recommendationTypeMap).map((key) => ([key, {
+    courses: [],
+    title: recommendationTypeMap[key].title,
+    description: recommendationTypeMap[key].description,
+  }])));
+  const courses = new Set([]);
+
+
+  for (const [key, value] of Object.entries(recommendationTypeMap)) {
+    const recs = value.func(aggregateData, value.params.limit);
+    recs.forEach((courseID, index) => {
+      courses.add(courseID);
+      rankedCourses[key].courses.push({ courseID, index });
+    });
+  }
+
+  const courseInfo = await getCoursesInformation(Array.from(courses));
+  const courseInfoMap = new Map(courseInfo.map((course) => [course.courseID, course]));
+
+  /* Add additional information from aggregate */
+  aggregateData.forEach((course) => {
+    if (!courseInfoMap.has(course.courseID)) {
+      return;
+    }
+    const info = courseInfoMap.get(course.courseID);
+    info.numReviews = course.numReviews;
+    info.avgValue = course.avgValue;
+  });
+
+
+  Object.entries(rankedCourses).forEach(([key, value]) => {
+    value.courses = value.courses.map((course) => ({
+      ...course,
+      courseName: courseInfoMap.get(course.courseID)?.courseName,
+      courseDescription: courseInfoMap.get(course.courseID)?.courseDescription,
+      numReviews: courseInfoMap.get(course.courseID)?.numReviews,
+      avgValue: courseInfoMap.get(course.courseID)?.avgValue,
+    })) as public_course[];
+  });
+
+  res.status(200).end(JSON.stringify(rankedCourses));
 
 };
 
