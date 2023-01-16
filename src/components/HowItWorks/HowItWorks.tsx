59c2438070327be33619ec0d4d260de4f774739b
Nicholas Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2023-01-16 09:11:30 -0500

    Bugfix: fix CLS on homepage

--- src/components/HowItWorks/HowItWorks.tsx
+++ src/components/HowItWorks/HowItWorks.tsx
@@ -5,6 +5,7 @@ import Feature from "../common/Feature";
 import FlexGroup from "../common/FlexGroup";
 import Tile from "../Tile";
 import styles from "./HowItWorks.module.scss";
+import Image from "next/image";
 
 
 function HowItWorks({ }) {
@@ -13,31 +14,9 @@ function HowItWorks({ }) {
     return (
         <div className={styles.container}>
             <Center><h2>How it works</h2></Center>
-            {/* <FlexGroup>
-                <Feature> 
-                    <FaBook />
-                    <h3>Browse Courses</h3>
-                    <p>
-                        MiddCourses is Middlebury&apos;s premier course discovery and review platform.  Browse from our complete catalogue to discover the perfect course for you.
-                    </p>
-                </Feature>
-                <Feature> 
-                    <FaBuilding />
-                    <h3>Discover Departments</h3>
-                    <p>Find your perfect Major or Minor with ease. And within departments, discover top courses.</p>
-                </Feature>
-                <Feature> 
-                    <FaUserGraduate />
-                    <h3>Meet Professors</h3>
-                    <p>
-                        Find accommodating, knowledgeable, and enthusiastic instructors for your next course.  Your new favorite instructor is here.
-                    </p>
-                </Feature>
-            </FlexGroup > */}
-
             <FlexGroup>
                 <Tile>
-                    <img src="/images/take-a-survey.svg" alt="1" width={390} height={260} />
+                    <Image src="/images/take-a-survey.svg" alt="1" width={390} height={260} />
                     <RiNumber1 className={styles.tileIcon} />
                     <h3 className={styles.title}>Review 2 Courses</h3>
                     <p className={styles.description}>
@@ -46,7 +25,7 @@ function HowItWorks({ }) {
 
                 </Tile>
                 <Tile>
-                    <img src="/images/data-work.svg" alt="1" width={390} height={260} />
+                    <Image src="/images/data-work.svg" alt="1" width={390} height={260} />
                     <RiNumber2 className={styles.tileIcon} />
                     <h3 className={styles.title}>Pick What&apos;s Right for You</h3>
                     <p className={styles.description}>
@@ -55,7 +34,7 @@ function HowItWorks({ }) {
 
                 </Tile>
                 <Tile>
-                    <img src="/images/check-for-a-fit.svg" alt="1" width={390} height={260} />
+                    <Image src="/images/check-for-a-fit.svg" alt="1" width={390} height={260} />
                     <RiNumber3 className={styles.tileIcon} />
                     <h3 className={styles.title}>Discover Recommended Courses</h3>
                     <p className={styles.description}>
