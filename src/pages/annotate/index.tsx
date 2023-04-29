bca201b49414e2c94002c62e79f2e3bd55df96ba
Nicholas Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2023-04-29 14:10:06 -0400

    Bugfix: proper pagination for annotations

--- src/pages/annotate/index.tsx
+++ src/pages/annotate/index.tsx
@@ -1,9 +1,10 @@
 import { getSession } from "next-auth/react";
 import { CustomSession, public_instructor, public_review } from "../../lib/common/types";
-import { getNRandomUnvotedReviews } from "../../lib/backend/database/review";
+import { getMaxReviewPages, getNRandomUnvotedReviews } from "../../lib/backend/database/review";
 import ReviewList from "../../components/Review";
 import { getAllInstructors } from "../../lib/backend/database/instructor";
 import { useRouter } from "next/router";
+import PageTitle from "../../components/common/PageTitle";
 
 export async function getServerSideProps(context) {
     const session = await getSession(context) as CustomSession;
@@ -11,7 +12,9 @@ export async function getServerSideProps(context) {
     const num = context.query.num ? parseInt(context.query.num as string) : 50;
     const page = context.query.page ? parseInt(context.query.page as string) : 0;
 
-    if (isNaN(num) || isNaN(page) || num < 1 || page < 0) {
+    const offset = page * num;
+
+    if (isNaN(num) || isNaN(page) || num < 1 || page < 0 || offset < 0) {
         return {
             redirect: {
                 destination: "/annotate",
@@ -30,7 +33,7 @@ export async function getServerSideProps(context) {
     }
 
 
-    const reviews = await getNRandomUnvotedReviews(session, num, page);
+    const reviews = await getNRandomUnvotedReviews(session, num, offset);
     const instructors = await getAllInstructors();
 
     return {
@@ -68,20 +71,23 @@ export function Annotate({ reviews, instructors }: AnnotateProps) {
     }
 
     return (
-        <div>
-            <h1>Annotate</h1>
-            <button onClick={prevPage} disabled={!hasPrevPage}>Previous Page</button>
-            <button onClick={nextPage}>Next Page</button>
-            <ReviewList
-                reviews={reviews}
-                identifyCourse
-                identifyInstructor
-                instructors={instructors}
-            />
-            <button onClick={prevPage} disabled={!hasPrevPage}>Previous Page</button>
-            <button onClick={nextPage}>Next Page</button>
-            <div style={{ height: "100px" }} />
-        </div>
+        <>
+            <PageTitle pageTitle="Annotate" />
+            <div>
+                <h1>Annotate</h1>
+                <button onClick={prevPage} disabled={!hasPrevPage}>Previous Page</button>
+                <button onClick={nextPage}>Next Page</button>
+                <ReviewList
+                    reviews={reviews}
+                    identifyCourse
+                    identifyInstructor
+                    instructors={instructors}
+                />
+                <button onClick={prevPage} disabled={!hasPrevPage}>Previous Page</button>
+                <button onClick={nextPage}>Next Page</button>
+                <div style={{ height: "100px" }} />
+            </div>
+        </>
     );
 
 
